873c1d86f42e54580d537c87f4ff006eb699e311
hxebolax <xebolax@gmail.com>
2025-01-16 10:22:32 +0100

    Preparaci├│n  versi├│n 1.4 y NVDA 2025.1

--- buildVars.py
+++ buildVars.py
@@ -41,7 +41,7 @@ Asignar teclas en Gestos de entrada / Visor de consola."""),
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2022.1.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2024.4.1",
+	"addon_lastTestedNVDAVersion": "2025.1.0",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
--- run_git.bat
+++ run_git.bat
@@ -4,8 +4,8 @@ scons --clean
 scons pot
 git init
 git add --all
-git commit -m "Versión 1.3"
+git commit -m "Preparación  versión 1.4 y NVDA 2025.1"
 git push -u origin master
-git tag 1.3
-git push --tags
+#git tag 1.3
+#git push --tags
 pause
